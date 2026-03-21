3a9e4301b81b51a79f9db5173ef13fb1efc509a6
Morena <morenicora.dev@gmail.com>
2026-03-21 13:34:17 +0000

    model client

--- src/api/models.py
+++ src/api/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
+from sqlalchemy import String, Boolean, DateTime
 from sqlalchemy.orm import Mapped, mapped_column
+from datetime import datetime, timezone
 
 db = SQLAlchemy()
 
@@ -16,4 +17,19 @@ class User(db.Model):
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
-        }
+        }   
+
+db = SQLAlchemy()
+    
+class Client(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(String(60), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(nullable=False)
+    sign_up_date: Mapped[datetime] = mapped_column(DateTime(), nullable=False, default=lambda: datetime.now(timezone.utc))
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            "sign_up_date": self.sign_up_date.isoformat()
+        }        
