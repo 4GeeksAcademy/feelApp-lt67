4819584d438382f5d7afc468907759f6c3a9f44f
Morena <morenicora.dev@gmail.com>
2026-04-05 23:35:37 +0000

    changed shared component access for better access

--- src/front/pages/client/AccessPage.jsx
+++ src/front/pages/client/AccessPage.jsx
@@ -166,12 +166,6 @@ const AccessPage = () => {
         >
           <i className="bi bi-person-badge me-2"></i>Coaches
         </button>
-        <button
-        className={`btn btn-forum-switch rounded-pill px-4 ${view === "shared" ? "active" : ""}`}
-        onClick={() => navigate("/shared")}
-      >
-        <i className="bi bi-heart me-2"></i>Shared
-      </button>
       </div>
 
       {view === "clients" && (
