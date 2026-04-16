0fbc4ad49b5d8b72b716c0202703372fac4f699e
Morena <morenicora.dev@gmail.com>
2026-04-16 12:27:38 -0300

    Remove alembic_version table drop command
    Removed command to drop alembic_version table before upgrade.

--- render_build.sh
+++ render_build.sh
@@ -9,8 +9,6 @@ pip install pipenv
 
 pipenv install
 
-pipenv run python -c "from src.app import app; from api.models import db; with app.app_context(): db.session.execute('DROP TABLE IF EXISTS alembic_version CASCADE'); db.session.commit(); print('Alembic table dropped')"
-
 pipenv run upgrade
 #!/usr/bin/env bash
 set -o errexit
