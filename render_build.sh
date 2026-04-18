8ee87407d12819ed95fc5b3a295d54b777d5b8c6
Morena <morenicora.dev@gmail.com>
2026-04-18 19:40:57 +0000

    pm

--- render_build.sh
+++ render_build.sh
@@ -6,9 +6,11 @@ npm install
 npm run build
 
 pip install pipenv
-
 pipenv install
 
-pipenv run flask db stamp head || true
+export PYTHONPATH=$PYTHONPATH:$(pwd)/src
+export FLASK_APP=src/app.py
+
+pipenv run python -c "from app import app; from api.models import db; app.app_context().push(); db.session.execute(db.text('DROP TABLE IF EXISTS alembic_version CASCADE')); db.session.commit()"
 
 pipenv run flask db upgrade
